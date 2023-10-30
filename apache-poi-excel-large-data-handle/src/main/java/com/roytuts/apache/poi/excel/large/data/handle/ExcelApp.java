5486c7682bc12a146404cba894cc0ddf5adbbb56
Soumitra <projwebinfo@gmail.com>
2023-10-30 17:08:55 +0530

    Update ExcelApp.java

--- apache-poi-excel-large-data-handle/src/main/java/com/roytuts/apache/poi/excel/large/data/handle/ExcelApp.java
+++ apache-poi-excel-large-data-handle/src/main/java/com/roytuts/apache/poi/excel/large/data/handle/ExcelApp.java
@@ -8,14 +8,14 @@ public class ExcelApp {
 	public static void main(String[] args) {
 		ExcelMockData mockData = new ExcelMockData();
 		List<ExcelVo> excelData = mockData.getExcelData();
-		
+
 		// using auto flush mode
 		final long autoFlushStartTime = System.currentTimeMillis();
-		String fileName = "C:/eclipse_2020_12_R/apache-poi-excel-large-data-handle/Excel-auto-flush.xlsx";
-		
+		String fileName = "C:/eclipse-workspace/apache-poi-excel-large-data-handle/Excel-auto-flush.xlsx";
+
 		ExcelWriterAutoFlush autoFlush = new ExcelWriterAutoFlush();
 		autoFlush.writeToExcelAutoFlush(fileName, excelData);
-		
+
 		final long autoFlushEndTime = System.currentTimeMillis();
 		final long autoFlushExeTime = autoFlushEndTime - autoFlushStartTime;
 		final long hr = TimeUnit.MILLISECONDS.toHours(autoFlushExeTime);
@@ -25,18 +25,18 @@ public class ExcelApp {
 				- TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(autoFlushExeTime));
 		final long ms = TimeUnit.MILLISECONDS.toMillis(autoFlushExeTime)
 				- TimeUnit.SECONDS.toMillis(TimeUnit.MILLISECONDS.toSeconds(autoFlushExeTime));
-		
+
 		System.out.println(String.format(
 				"Total time taken to execute 20000 records using auto flush: %d Hours %d Minutes %d Seconds %d Milliseconds",
 				hr, min, sec, ms));
-		
+
 		// using manual flush mode
 		final long manualFlushStartTime = System.currentTimeMillis();
-		
-		fileName = "C:/eclipse_2020_12_R/apache-poi-excel-large-data-handle/Excel-manual-flush.xlsx";
+
+		fileName = "C:/eclipse-workspace/apache-poi-excel-large-data-handle/Excel-manual-flush.xlsx";
 		ExcelWriterManualFlush manualFlush = new ExcelWriterManualFlush();
 		manualFlush.writeToExcelManualFlush(fileName, excelData);
-		
+
 		final long manualFlushEndTime = System.currentTimeMillis();
 		final long manualFlushExeTime = manualFlushEndTime - manualFlushStartTime;
 		final long manualFlushHr = TimeUnit.MILLISECONDS.toHours(manualFlushExeTime);
@@ -46,7 +46,7 @@ public class ExcelApp {
 				- TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(manualFlushExeTime));
 		final long manualFlushMs = TimeUnit.MILLISECONDS.toMillis(manualFlushExeTime)
 				- TimeUnit.SECONDS.toMillis(TimeUnit.MILLISECONDS.toSeconds(manualFlushExeTime));
-		
+
 		System.out.println(String.format(
 				"Total time taken to execute 20000 records using manual flush: %d Hours %d Minutes %d Seconds %d Milliseconds",
 				manualFlushHr, manualFlushMin, manualFlushSec, manualFlushMs));
